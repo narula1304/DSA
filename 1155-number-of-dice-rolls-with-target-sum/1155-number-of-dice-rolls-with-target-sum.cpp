10e4d1cabc6fa97350182dcd7d89e02b30a772d9
PRINCE NARULA <princenarula1313@gmail.com>
2026-07-11 11:47:04 +0530

    Time: 3 ms (98.75%), Space: 8 MB (93.73%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[31][1001];
+    int mod = 1e9+7;
+
+    int solve(int n,int k,int target){
+        if(target < 0) return 0;
+        if(n == 0) return (target == 0);
+
+        if(dp[n][target] != -1) return dp[n][target];
+
+        int ways = 0;
+        for(int face=1;face<=k;face++){
+            ways = (ways + solve(n-1,k,target-face) )% mod;
+        }
+
+        return dp[n][target] =  ways;
+    }
+
+    int numRollsToTarget(int n, int k, int target) {
+        memset(dp,-1,sizeof(dp));
+
+        return solve(n,k,target);
+    }
+};
