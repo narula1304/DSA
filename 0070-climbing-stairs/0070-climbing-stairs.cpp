35518a6d22d844ac06f64904b7bca29ab63621d2
PRINCE NARULA <princenarula1313@gmail.com>
2026-08-14 19:14:37 +0530

    Time: 0 ms (100%), Space: 8.5 MB (39.05%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int calc(int n , vector<int>& dp){
+        if(n<=1) return 1;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = calc(n-1 , dp) + calc(n-2 , dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1 , -1);
+        int ans = calc(n,dp);
+        return ans;
+    }
+};
