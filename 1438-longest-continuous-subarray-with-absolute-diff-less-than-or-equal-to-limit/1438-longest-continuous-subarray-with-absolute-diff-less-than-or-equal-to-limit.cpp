ab284add7075272db4d0b2e2e8ce83a3376481cb
PRINCE NARULA <princenarula1313@gmail.com>
2026-09-04 15:33:46 +0530

    Time: 183 ms (10.14%), Space: 117.9 MB (7.26%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -2,41 +2,22 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
         int maxlen = 0;
+        int left = 0;
 
-        int left =  0;
-        int right = 0;
+        multiset<int> st;
 
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minheap;
-        priority_queue<pair<int,int>> maxheap;
+        for (int right = 0; right < nums.size(); right++) {
 
-        while(right < nums.size()){
-            minheap.push({nums[right],right});
+            st.insert(nums[right]);
 
-            maxheap.push({nums[right],right});
-
-            while(maxheap.top().first - minheap.top().first > limit){
-                int ind = min(minheap.top().second,maxheap.top().second);
-                left = ind + 1;
-
-                while(maxheap.top().second < left){
-                    maxheap.pop();
-                }
-
-                while(minheap.top().second < left){
-                    minheap.pop();
-                }
+            while (*st.rbegin() - *st.begin() > limit) {
+                st.erase(st.find(nums[left]));
+                left++;
             }
 
-            maxlen = max(maxlen,right-left+1);
-
-           
-
-            right++;
-
-
+            maxlen = max(maxlen, right - left + 1);
         }
 
         return maxlen;
-
     }
 };
